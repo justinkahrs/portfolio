3589c9e4447d644f5732ae1324e05b1acdedb185
Justin Kahrs <justin@justinkahrs.com>
2017-06-18 20:45:03 -0400

    remove react-router, overkill

--- src/components/common/navigation/Navigation.js
+++ src/components/common/navigation/Navigation.js
@@ -2,38 +2,70 @@ import React from 'react';
 import Work from '../../work/Work';
 import Contact from '../../contact/Contact';
 import About from '../../about/About';
-import { Col, Nav, NavItem } from 'react-bootstrap';
-import { LinkContainer } from 'react-router-bootstrap';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { Col, Collapse, Nav, NavItem } from 'react-bootstrap';
 import './navigation.css';
 
 const styles = {
   nav: {
     paddingBottom: '2em',
+  },
+};
+
+const Pages = {
+  about: <About key="about" />,
+  contact: <Contact key="contact" />,
+  work: <Work key="work" />,
+};
+
+class Navigation extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      open: false,
+      selected: <div />,
+    };
+    this.handleSelect = this.handleSelect.bind(this);
+  }
+
+  handleSelect(selection) {
+    const { selected, open } = this.state;
+    if (selected !== Pages[selection]) {
+      this.setState({
+        open: true,
+        selected: Pages[selection],
+      });
+    } else {
+      this.setState({
+        open: !open,
+        selected: Pages[selection],
+      });
+    }
+  }
+
+  render() {
+    return (
+      <div className="fade-in third">
+        <Col lg={12}>
+          <Nav
+            style={styles.nav}
+            justified
+            bsStyle="tabs"
+            onSelect={this.handleSelect}
+          >
+            <NavItem eventKey={'about'}>About</NavItem>
+            <NavItem eventKey={'work'}>Work</NavItem>
+            <NavItem eventKey={'contact'}>Contact</NavItem>
+          </Nav>
+        </Col>
+        <Col lg={12}>
+          <Collapse in={this.state.open}>
+            <div>
+              {this.state.selected}
+            </div>
+          </Collapse>
+        </Col>
+      </div>
+    );
   }
 }
-const handleSelect = selected => (
-  console.log(`Selected: ${selected}`)
-);
-
-const Navigation = () => (
-  <Router>
-    <div className="fade-in third">
-      <Col lg={12}>
-        <Nav style={styles.nav} justified bsStyle="tabs" onSelect={handleSelect}>
-          <LinkContainer to="/about"><NavItem eventKey={'about'}>About</NavItem></LinkContainer>
-          <LinkContainer to="/work"><NavItem eventKey={'work'}>Work</NavItem></LinkContainer>
-          <LinkContainer to="/contact"><NavItem eventKey={'contact'}>Contact</NavItem></LinkContainer>
-        </Nav>
-      </Col>
-      <Col lg={12}>
-        <div>
-          <Route path="/about" component={About} />
-          <Route path="/work" component={Work} />
-          <Route path="/contact" component={Contact} />
-        </div>
-      </Col>
-    </div>
-  </Router>
-);
 export default Navigation;

ddbec3516f873f9e82954ea92b3632c320880e35
avanderj <avanderj@indiana.edu>
2017-07-08 14:55:26 -0400

    Update Work.js

--- src/components/work/Work.js
+++ src/components/work/Work.js
@@ -29,18 +29,18 @@ const Work = () => (
       }
     >
       <p>
-        Work with agile team to replace a legacy business intelligence
+		<ul>
+			<li>Work with agile team to replace a legacy business intelligence
         reporting portal with one that uses modern technologies, is more flexible to changes
         in business logic, and has a more visually appealing style. Build the backend using Spring Boot securing
-        each endpoint with JWT and OAuth2Work within the boundaries of our existing infrastructure.
-      </p>
-      <p>
-        Implement webpack for build automation tasks and React as
-        the interface for the frontend application. Set up automated functional testing for the project using a script to start up an xvfb instance (headless browser testing) with Nightwatch.js on Bamboo CI servers.
-      </p>
-      <p>
-        Work closely with the UX/Design team to fulfill the primary requirements, especially in terms of usability for customers.
-      </p>
+        each endpoint with JWT and OAuth2Work within the boundaries of our existing infrastructure.</li>
+
+			<li>Implement webpack for build automation tasks and React as
+        the interface for the frontend application. Set up automated functional testing for the project using a script to start up an xvfb instance (headless browser testing) with Nightwatch.js on Bamboo CI servers.</li>
+
+			<li>Work closely with the UX/Design team to fulfill the primary requirements, especially in terms of usability for customers.</li>
+		</ul>
+	  </p>
     </PromotionEntry>
     <Entry
       location="Grand Valley State University - Allendale, MI"
@@ -49,28 +49,24 @@ const Work = () => (
       endDate="June 2015"
     >
       <p>
-        Handled installation and provisioning of all new servers. Connected
-        any necessary disk shelves. Installed RAID/Fiber Channel HBAs, and upgraded CPU/RAM when necessary.
-      </p>
-      <p>
-        Responsible for the HP System Insight Manager server and entrusted to the well-being
+		<ul>
+			<li>Handled installation and provisioning of all new servers. Connected
+        any necessary disk shelves. Installed RAID/Fiber Channel HBAs, and upgraded CPU/RAM when necessary.</li>
+		
+			<li>Responsible for the HP System Insight Manager server and entrusted to the well-being
         of every server in the Data Center. Added all of the HP servers to this system (300+) and expanded the
-         system as the network grew.
-      </p>
-      <p>
-        In charge of testing the security of the networks. Used BeyondTrust’s Retina Network Security Scanner,
+         system as the network grew.</li>
+
+			<li>In charge of testing the security of the networks. Used BeyondTrust’s Retina Network Security Scanner,
         NMAP and other tools. Compiled detailed vulnerability reports and assessed threats starting with the most
         pertinent (at the time, Heartbleed/Shellshock exploits) while working directly with the system administrator
-        to find the best course of remediation.
-      </p>
-      <p>
-		<ul>
-			<li></li>
-		</ul>
-        Maintained servers in the private secured environment and ensured they were all compliant with DSS 3.0
+        to find the best course of remediation.</li>
+		
+			<li>Maintained servers in the private secured environment and ensured they were all compliant with DSS 3.0
         in order to process credit card information. Acquired some practice with PII (Personally Identifying Information)
         scanning, which looked for personal information such as SSNs on the network shares to reduce risk of
-        sensitive data loss.
+        sensitive data loss.</li>
+		</ul>
       </p>
     </Entry>
   </Col>

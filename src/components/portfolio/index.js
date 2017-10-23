c1f3aa17b95c60399247cd5f41d370dfbfd5cccd
Justin Kahrs <justin@justinkahrs.com>
2017-10-23 05:50:11 -0400

    typo fix

--- src/components/portfolio/index.js
+++ src/components/portfolio/index.js
@@ -21,10 +21,9 @@ const Portfolio = () => (
     </a>
     <br />
     <p style={{ textAlign: 'justify' }}>
-      This is a proof of concept website I made to simply the PC building process. The site uses React 16, React Router
-      v4, and Redux as the way to keep track of the shopping cart. I really tried to focus on a modern interface with
-      easy an easy to follow form. See the code for Pick a Part PC and the site you're currently reading on my{' '}
-      <a href="https://github.com/justinkahrs">github page</a>.
+      This is a proof of concept website I made to simplify the PC building process. The site uses React 16, React Router
+      v4, and Redux (to keep track of the shopping cart). I focused on a modern interface with
+      an easy to follow form. See the code for Pick a Part PC and the site you're currently reading on my <a href="https://github.com/justinkahrs">github page</a>.
     </p>
   </Col>
 );

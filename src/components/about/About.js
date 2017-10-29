2a7fa94013e7d7d0ff066ff95d033179aec019ca
Justin Kahrs <justin@justinkahrs.com>
2017-10-29 17:21:56 -0400

    final changes for the day

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -8,7 +8,7 @@ import './about.css';
 
 const About = () => (
   <Col className="aboutPage" lg={12}>
-    <Col className="profilePic" xs={12} sm={6} md={5} lg={4}>
+    <Col className="profilePic" xs={12} sm={6} md={6} lg={4}>
       <Image alt="me in front of a blue background" src={profilePic} responsive rounded />
     </Col>
     {profileText}

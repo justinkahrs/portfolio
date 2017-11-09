7a32a8401e50188449be28c661a0bfddb2ffe0ff
Justin Kahrs <justin@justinkahrs.com>
2017-11-09 06:08:07 -0500

    remove stupid animations

--- src/App.js
+++ src/App.js
@@ -1,7 +1,5 @@
 import React, { PureComponent } from 'react';
 import { Grid, Row } from 'react-bootstrap';
-import { bounceInUp } from 'react-animations';
-import styled, { keyframes } from 'styled-components';
 
 import Header from './components/common/header/Header';
 import Navigation from './components/common/navigation/Navigation';
@@ -9,9 +7,6 @@ import Pages from './components/common/navigation/Pages';
 import NavItems from './components/common/navigation/NavItems';
 
 import './app.css';
-
-const bounceAnimation = keyframes`${bounceInUp}`;
-const BouncyDiv = styled.div`animation: 1s ${bounceAnimation};`;
 
 class App extends PureComponent {
   constructor(props) {
@@ -35,10 +30,8 @@ class App extends PureComponent {
           <Header main="Justin Kahrs" sub="Software Engineer" />
         </Row>
         <div className="navAndBody">
-          <BouncyDiv className="all">
-            <Navigation handleSelect={this.handleSelect} navItems={NavItems} />
-            {selected}
-          </BouncyDiv>
+          <Navigation handleSelect={this.handleSelect} navItems={NavItems} />
+          {selected}
         </div>
       </Grid>
     );
--- src/components/common/header/Header.js
+++ src/components/common/header/Header.js
@@ -1,18 +1,10 @@
 import React from 'react';
-import { bounceInLeft, bounceInRight } from 'react-animations';
-import styled, { keyframes } from 'styled-components';
-
 import './header.css';
-
-const bounceLeftAnimation = keyframes`${bounceInLeft}`;
-const bounceRightAnimation = keyframes`${bounceInRight}`;
-const BouncyLeft = styled.div`animation: 1s ${bounceLeftAnimation};`;
-const BouncyRight = styled.div`animation: 1s ${bounceRightAnimation};`;
 
 const Header = props => (
   <div className="header">
-    <BouncyLeft className="header__main">{props.main}</BouncyLeft>
-    <BouncyRight className="header__sub">{props.sub}</BouncyRight>
+    <div className="header__main">{props.main}</div>
+    <div className="header__sub">{props.sub}</div>
   </div>
 );
 export default Header;

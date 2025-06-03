a46d66e1541b3982660c3a641601b486920347e5
Justin Kahrs <justin@justinkahrs.com>
2025-06-03 13:36:30 -0400

    update resume link and turn off auto play

--- src/app/music/page.tsx
+++ src/app/music/page.tsx
@@ -8,10 +8,11 @@ export default function Music() {
       {/* SoundCloud Embed */}
       <div className="aspect-w-16 aspect-h-9">
         <iframe
+          title="soundcloud"
           width="100%"
           height="166"
-          src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/1458463900&color=%23ff5500&auto_play=true&hide_related=true&show_comments=true&show_user=true&show_reposts=false&show_teaser=false"
-        ></iframe>
+          src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/1458463900&color=%23ff5500&auto_play=false&hide_related=true&show_comments=true&show_user=true&show_reposts=false&show_teaser=false"
+        />
         <p className="text-gray-500 text-center text-xs mt-2">
           <a
             href="https://soundcloud.com/justinkahrs"
@@ -35,6 +36,7 @@ export default function Music() {
 
       {/* Spotify Embed */}
       <iframe
+        title="spotify"
         style={{ borderRadius: "12px" }}
         src="https://open.spotify.com/embed/track/4S9KLaUwqvORgG1gqbUadD?utm_source=generator&theme=0"
         width="100%"
@@ -42,7 +44,7 @@ export default function Music() {
         frameBorder="0"
         allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
         loading="lazy"
-      ></iframe>
+      />
     </div>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,22 +13,30 @@ export default function Work() {
           <li>
             AI and automation consulting via{" "}
             <a
-              href="https://sennaautomation.agency"
+              href="https://www.senna-automation.com"
               target="_blank"
               rel="noopener noreferrer"
               className="text-blue-500 hover:underline"
             >
-              my automation agency
+              Senna Automation
             </a>
           </li>
           <li>Web and application development contracting</li>
           <li>Sound design for games and other commercial projects</li>
         </ul>
         <p className="text-gray-600 mt-2">
-          Feel free to{" "}
+          Feel free to
+          <Link
+            href="https://justinkahrs.github.io/resume/"
+            passHref
+            legacyBehavior
+          >
+            <a className="text-blue-500 hover:underline"> read my resume </a>
+          </Link>
+          or
           <Link href="/contact" passHref legacyBehavior>
-            <a className="text-blue-500 hover:underline">contact me</a>
-          </Link>{" "}
+            <a className="text-blue-500 hover:underline"> contact me </a>
+          </Link>
           to discuss how we can work together.
         </p>
       </div>
